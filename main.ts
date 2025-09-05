8615bc070c5aa5d63c6902e89bd96424ffe62eba
Bor3y9 <yousef161247@eng.psu.edu.eg>
2025-09-05 18:30:00 +0300

    feat:Making the weather api

---
+++ main.ts
@@ -0,0 +1,46 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+
+const server = new McpServer({
+  name: "weather-mcp-server",
+  version: "1.0.0",
+});
+
+server.registerTool(
+  "fetch-weather",
+  {
+    title: "Weather Fetcher",
+    description: "Get weather data for a city",
+    inputSchema: { city: z.string() },
+  },
+  async ({ city }) => {
+    const data = await fetch(
+      `https://geocoding-api.open-meteo.com/v1/search/?name=${city}`
+    );
+    if (data.status !== 200) {
+      throw new Error(`Failed to fetch coordinates for city: ${city}`);
+    }
+    const geoJson = await data.json();
+    const { longitude, latitude } = geoJson.results[0];
+
+    const response2 = await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`
+    );
+    if (response2.status !== 200) {
+      throw new Error(`Failed to fetch weather data for city: ${city}`);
+    }
+    const weatherData = await response2.json();
+    return {
+      content: [
+        {
+          type: "text",
+          text: JSON.stringify(weatherData, null, 2),
+        },
+      ],
+    };
+  }
+);
+
+const transport = new StdioServerTransport();
+server.connect(transport);
